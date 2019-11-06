0faf2d9f9736430940142bbfc39c1c2660336473
galaxyunicorn42 <marisol.vargas@amethodschools.org>
2019-11-06 22:04:26 +0000

    rockpaperscissors

--- js/script.js
+++ js/script.js
@@ -8,10 +8,32 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+let userChoice =""
+let computerChoice =""
+let randomChoice =0
 $('document').ready(function() {
+ randomChoice= math.random();
 $("#shoot").click(function(){
     let input= $("#input").val();
     $("#userChoice").text(input);
     console.log(input);
 });
 });
+if(randomNumber>.66){
+    computerChoice= "rock";
+}
+else if(randomNumber>.33){
+    computerChoice ="paper";
+}
+else{
+    computerChoice="scissors";
+}
+console.log(computerChoice)
+$("#computerChoice").html(computerChoice);
+if (userChoice === computerChoice){
+    console.log("tied")
+    $("#result").html("tied!");
+}
+
+
+
